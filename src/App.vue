792086936802cf03b30888f14d1f69e696be946b
ezkam <kf.zakiyudin@gmal.com>
2022-11-11 09:17:03 +0700

    if-else, for-loop, data-binding, filter in computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <h5>Selamat datang: {{ name }}</h5>
+  <input type="text" v-model="name"><br>
+  <button v-on:click="ubahNama">Ubah Nama</button>
+
+  <p>------------------------------------------------</p>
+  <!-- filter search dari title -->
+  <input type="text" v-model="search">
+  <!-- end filter search -->
+
+  <div v-if="products.length !== 0">
+    <ul>
+      <li v-for="list in filterSearch" v-bind:key=list.id>
+        {{ list.title }} : {{ list.price }}
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <p>[empty]</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      search: '',
+      name: '',
+      products: []
+    }
+  },
+  methods: {
+    ubahNama() {
+      this.name = 'Kok nama ku diubah si 😑'
+    }
+  },
+  created() {
+    this.products = [
+        { id: 1, title: "Product 1", price: 5000 },
+        { id: 2, title: "Product 2", price: 4500 },
+        { id: 3, title: "Product 3", price: 3700 },
+        { id: 4, title: "Product 4", price: 1200 },
+    ]
+  },
+    // * using computed properties
+  computed: {
+    // * method untuk me-filter by title
+    filterSearch() {
+      return this.products.filter(product => {
+        return product.title.match(this.search)
+      })
+    }
   }
 }
 </script>
